a0ae6be3f7246bdc8c9b2485802b3d09b9030830
cleoputra <acesam222@gmail.com>
2021-07-25 14:56:48 +0700

    photo

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,8 +1,6 @@
-import Image from "next/image";
-
 function Avatar({url, className}) {
     return (
-        <Image 
+        <img
         loading="lazy"
         src={url}
         className={`h-10 rounded-full cursor-pointer transition duration-150 transform hover:scale-110 ${className}`}
